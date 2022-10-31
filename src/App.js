11af7a5266d53ffa1598bf24cebc64d454ab2889
Junayed5 <mdjunayed@gmail.com>
2022-10-31 11:47:34 +0600

    list success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h2 className='text-center text-info fw-bold p-4'>All Users</h2>
+
+      <Home/>
     </div>
   );
 }
